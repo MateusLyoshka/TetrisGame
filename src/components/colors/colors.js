83ef477ad5cf6ed90bbf8499bd8a7c811d342119
Mateus santos fernandes <mateusf.2022@alunos.utfpr.edu.br>
2024-07-15 16:49:22 -0300

    first commit

---
+++ src/components/colors/colors.js
@@ -0,0 +1,22 @@
+const COLORS = () => [
+            
+            "#2a59b5",
+            "#6ae215",
+            "#e4a89f",
+            "#a785ef",
+            "#5df26a",
+            "#a4ea4d",
+            "#a26ed2",
+            "#fab598"
+        ]
+
+    
+
+export default COLORS
+
+// const Renderblock = () => {
+//     for (let i = 0; i < Colors.length; i++) {
+        
+        
+//     }
+// }
